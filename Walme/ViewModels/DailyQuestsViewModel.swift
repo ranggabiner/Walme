ddd3d9cb23986a1f0e6deabf991f714d0a683571
DasonTiovino <114289111+DasonTio@users.noreply.github.com>
2024-06-21 12:20:21 +0700

    feat: daily quest completed

--- Walme/ViewModels/DailyQuestsViewModel.swift
+++ Walme/ViewModels/DailyQuestsViewModel.swift
@@ -1,7 +1,41 @@
 //
-//  DailyQuestsViewModel.swift
-//  Walme
+//  DailyQuestViewModel.swift
+//  walkapp
 //
-//  Created by Rangga Biner on 18/06/24.
+//  Created by Dason Tiovino on 18/06/24.
 //
 
+import Foundation
+import SwiftUI
+import Combine
+
+class DailyQuestsViewModel: ObservableObject {
+    @Published var isGoalCompleted: Bool = false
+    @Published var stepCountGoal: Double = 2000.0
+    @Published var stepCount: Double = 0.0
+    @Published var selectedDate: Date = Date()
+    
+    private var healthStore = HealthService()
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        fetchStepCount(for: selectedDate)
+        
+        $selectedDate
+            // subscribe selectedDate & run function if selectedDate changed
+            .sink { [weak self] newDate in
+                self?.fetchStepCount(for: newDate)
+            }
+            // prevent memory leaks
+            .store(in: &cancellables)
+    }
+    
+    private func fetchStepCount(for date: Date) {
+        healthStore.fetchStepCount(for: date) { [weak self] count in
+            DispatchQueue.main.async {
+                self?.stepCount = count
+                self?.isGoalCompleted = count >= self?.stepCountGoal ?? 2000.0
+            }
+        }
+    }
+}
